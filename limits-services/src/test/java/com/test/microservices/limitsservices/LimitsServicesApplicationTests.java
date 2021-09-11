5e1dd171ceee49c0dada92a3c29020dc81d5fc34
khalil <elmaghraoui.khalil.1@gmail.com>
2021-09-11 13:11:24 +0100

    service-limits

---
+++ limits-services/src/test/java/com/test/microservices/limitsservices/LimitsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.microservices.limitsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
