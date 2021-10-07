775f1f51e79275b1e37337e6da8cae5cd7908156
khalil <elmaghraoui.khalil.1@gmail.com>
2021-10-08 00:16:19 +0100

    api-gateway

--- currency-conversion-service/src/main/java/com/test/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/test/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,8 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//name of the service we want to call
-@FeignClient(name = "currency-exchange",url = "localhost:8000")
+//name of the service we want to call , without eureka
+//@FeignClient(name = "currency-exchange",url = "localhost:8000")
+//with Eureka
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currencyExchange/from/{from}/to/{to}")
