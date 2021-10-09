50eb328e8837dfea205bbeff50ed1faba25e9e2e
khalil <elmaghraoui.khalil.1@gmail.com>
2021-10-09 12:32:08 +0100

    api gateway

---
+++ api-gateway/src/main/java/com/test/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,10 @@
+package com.test.microservices.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    
+}
